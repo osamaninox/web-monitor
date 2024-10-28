2fa52eb5057a6d6e8a9f44816c705b094b5f1377
osamaninox <122790974+osamaninox@users.noreply.github.com>
2024-10-29 00:44:46 +0100

    Minor refactoring

--- web-url/web-controller.go
+++ web-url/web-controller.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"net/http"
 
+	"errors"
+
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
@@ -19,6 +21,7 @@ func NewWebController(logger *zap.Logger, database *gorm.DB) *WebController {
 }
 
 func (controller *WebController) AddWebUrlForMonitoring(w http.ResponseWriter, r *http.Request) {
+	var logger = controller.logger
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
@@ -35,28 +38,34 @@ func (controller *WebController) AddWebUrlForMonitoring(w http.ResponseWriter, r
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	controller.logger.Info("Web url to be added", zap.Any("webUrl", webUrl))
-	if webUrl.Url == "" {
-		controller.logger.Error("Url is required")
-		http.Error(w, "Url is required", http.StatusBadRequest)
+	err = validateWebUrl(webUrl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if webUrl.Interval == 0 {
-		controller.logger.Error("Interval is required")
-		http.Error(w, "Interval is required", http.StatusBadRequest)
-		return
-	}
+	logger.Info("Web url to be added", zap.Any("webUrl", webUrl))
+
 	result := controller.WebUrlRepository.CreateWebUrl(&webUrl)
 	if result.Error != nil {
-		controller.logger.Error("Error while adding web url", zap.Error(result.Error))
+		logger.Error("Error while adding web url", zap.Error(result.Error))
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
 	webUrlBytes, err := json.Marshal(webUrl)
 	if err != nil {
-		controller.logger.Error("Error while marshalling web url", zap.Error(err))
+		logger.Error("Error while marshalling web url", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Write(webUrlBytes)
+}
+
+func validateWebUrl(webUrl WebUrl) error {
+	if webUrl.Url == "" {
+		return errors.New("url is required")
+	}
+	if webUrl.Interval == 0 {
+		return errors.New("interval is required")
+	}
+	return nil
 }
--- web-url/web-url.repository.go
+++ web-url/web-url.repository.go
@@ -17,36 +17,39 @@ func (webUrlRepository *WebUrlRepository) CreateWebUrl(webUrl *WebUrl) (result *
 }
 
 func (webUrlRepository *WebUrlRepository) GetWebUrls() (groupedWebUrls map[int][]WebUrl, err error) {
+	var logger = webUrlRepository.logger
 	var webUrls []WebUrl
 	result := webUrlRepository.database.Find(&webUrls)
 	if result.Error != nil {
-		webUrlRepository.logger.Error("Error while fetching web urls", zap.Error(result.Error))
+		logger.Error("Error while fetching web urls", zap.Error(result.Error))
 		return nil, result.Error
 	}
 	groupedWebUrls = make(map[int][]WebUrl)
 	for _, webUrl := range webUrls {
 		groupedWebUrls[webUrl.Interval] = append(groupedWebUrls[webUrl.Interval], webUrl)
 	}
-	webUrlRepository.logger.Info("Grouped web urls", zap.Any("groupedWebUrls", groupedWebUrls))
+	logger.Info("Grouped web urls", zap.Any("groupedWebUrls", groupedWebUrls))
 	return groupedWebUrls, nil
 }
 
 func (webUrlRepository *WebUrlRepository) GetAllDistinctWebUrlIntervals() (intervals []int, err error) {
+	var logger = webUrlRepository.logger
 	result := webUrlRepository.database.Model(&WebUrl{}).Select("DISTINCT interval").Find(&intervals)
 	if result.Error != nil {
-		webUrlRepository.logger.Error("Error while fetching intervals", zap.Error(result.Error))
+		logger.Error("Error while fetching intervals", zap.Error(result.Error))
 		return nil, result.Error
 	}
-	webUrlRepository.logger.Info("Distinct intervals", zap.Any("intervals", intervals))
+	logger.Info("Distinct intervals", zap.Any("intervals", intervals))
 	return intervals, nil
 }
 
 func (webUrlRepository *WebUrlRepository) GetWebUrlsByInterval(interval int) (webUrls []WebUrl, err error) {
+	var logger = webUrlRepository.logger
 	result := webUrlRepository.database.Where("interval = ?", interval).Find(&webUrls)
 	if result.Error != nil {
-		webUrlRepository.logger.Error("Error while fetching web urls", zap.Error(result.Error))
+		logger.Error("Error while fetching web urls", zap.Error(result.Error))
 		return nil, result.Error
 	}
-	webUrlRepository.logger.Info(fmt.Sprintf("Web urls for interval %d", interval), zap.Any("webUrls", webUrls))
+	logger.Info(fmt.Sprintf("Web urls for interval %d", interval), zap.Any("webUrls", webUrls))
 	return webUrls, nil
 }
